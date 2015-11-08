379b39613bcb52dc477f4f79865d938915ada688
rasmus <rasmushofmann@gmail.com>
2015-11-08 13:43:42 +0100

    exercise 5.1: add toList

--- src/chapter5/Stream.scala
+++ src/chapter5/Stream.scala
@@ -5,7 +5,14 @@ sealed trait Stream[+A] {
     case Empty => None
     case Cons(h,t) => Some(h())
   }
+
+  def toList: List[A] = this match {
+    case Empty => Nil
+    case Cons(h, t) => h() :: t().toList
+  }
+
 }
+
 case object Empty extends Stream[Nothing]
 case class Cons[A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
---
+++ src/chapter5/Test.sc
@@ -0,0 +1,6 @@
+
+import chapter5._
+
+val stream = Stream(1,2,3)
+
+stream.toList
