7ce778a5df5cb1153b3a721e4f02fde2e51fbab0
Rasmus Kuschel <rasmushofmann@gmail.com>
2015-11-09 13:34:18 +0100

    exercise 6.5: positiveMax through map

--- src/chapter6/RNG.scala
+++ src/chapter6/RNG.scala
@@ -5,6 +5,19 @@ trait RNG {
 }
 
 object RNG {
+  type Rand[+A] = RNG => (A, RNG)
+
+  def unit[A](a: A): Rand[A] = rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] = rng => {
+    val (a, rng2) = s(rng)
+    (f(a), rng2)
+  }
+
+  def positiveMax(n: Int): Rand[Int] = map(positiveInt)(i => i % n)
+
+  // val int: Rand[Int] = _.nextInt
+
   def simple(seed: Long): RNG = new RNG {
     def nextInt = {
       val seed2 = (seed*0x5DEECE66DL + 0xBL) &
--- src/chapter6/RNGSpec.scala
+++ src/chapter6/RNGSpec.scala
@@ -19,12 +19,22 @@ class RNGSpec extends Specification {
       rndDouble must be_<=(1.0)
     }
 
+    // exercise 6.4
     "generate list of random ints" in {
       val rng = RNG.simple(System.currentTimeMillis())
       val is = RNG.ints(4)(rng)
 
       is._1.length must equalTo(4)
       is._2 must be_!=(rng)
+    }
+
+    // exercise 6.5
+    "positiveMax through map" in {
+      val rng = RNG.simple(System.currentTimeMillis())
+      val res = RNG.positiveMax(4)(rng)
+
+      res._1 must be_>=(0)
+      res._1 must be_<=(4)
     }
   }
 }
