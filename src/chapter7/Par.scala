63ffe187b69b2413a171f024229b29d8d28f0950
rasmus <rasmushofmann@gmail.com>
2015-11-14 15:16:18 +0100

    exercise 7.1. interface to Par.map2

--- src/chapter7/Par.scala
+++ src/chapter7/Par.scala
@@ -3,6 +3,9 @@ package chapter7
 class Par[A]
 
 object Par {
+
+  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = ???
+  
   def unit[A](a: A): Par[A] = ???
   def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = ???
   def fork[A](a: => Par[A]): Par[A] = ???
---
+++ src/chapter7/Test.sc
@@ -0,0 +1,4 @@
+import chapter7._
+
+val l,r: Par[Int] = ???
+val res = Par.map2(l, r)(_ + _)
