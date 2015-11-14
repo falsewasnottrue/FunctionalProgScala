8387aec23e10800c13513c1122a3c9fbadb5fc00
rasmus <rasmushofmann@gmail.com>
2015-11-14 16:25:05 +0100

    exercise 7.3: first implementation with ExecutorService

--- src/chapter7/Par.scala
+++ src/chapter7/Par.scala
@@ -1,14 +1,21 @@
 package chapter7
 
-class Par[A]
+import java.util.concurrent.ExecutorService
+
+import scala.concurrent.Future
+
 
 object Par {
 
-  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = ???
-  
-  def unit[A](a: A): Par[A] = ???
-  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = ???
-  def fork[A](a: => Par[A]): Par[A] = ???
+  type Par[A] = ExecutorService => Future[A]
+
+  def unit[A](a: A): Par[A] = _ => Future.successful(a)
+  def fork[A](a: => Par[A]): Par[A] = es => a(es)
+  def map2[A,B,C](a: Par[A], b: Par[B])(f: (A,B) => C): Par[C] = es => for {
+    resA <- a(es)
+    resB <- b(es)
+  } yield f(resA, resB)
+
   def async[A](a: => A): Par[A] = fork(unit(a))
-  def run[A](a: Par[A]): A = ???
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
 }
